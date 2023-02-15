0b408672f23de8961bfcf45f9d7d4b7684832a69
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-02-15 12:03:07 -0500

    This is my first code in c# :b

---
+++ MyFirstCodeOOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyFirstCodeOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try { 
+            var dateObject = new Date(0, 1, 30);
+            Console.WriteLine(dateObject);
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
