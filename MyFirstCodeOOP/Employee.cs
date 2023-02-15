abd57463b80d433859131e83c77397ddc983f232
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2023-02-15 18:36:21 -0500

    Creation new abstract class employee

---
+++ MyFirstCodeOOP/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    internal abstract class Employee
+    {
+        #region Variables
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }        
+        public Date BirthDay { get; set; }
+        public Date HiringDate { get; set; }
+        public bool IsActive { get; set; }
+        #endregion
+
+        #region Methods
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"------EMPLOYEE------\n\t ID: {Id}\n\t FIRST NAME: {FirstName} \n\t LAST NAME: {LastName} \n\t Hiring Date: {HiringDate} \n\t Is active: {IsActive}";
+        }
+        #endregion
+    }
+}
